bcf5a2fc92c02df396c0ad9558de1a4cb0697409
Wink Saville <wink@saville.com>
2015-06-07 11:09:07 -0700

    test_LinkFile.py is in the tools/tests directory.

--- test_LinkFile.py
+++ test_LinkFile.py
@@ -28,7 +28,7 @@ rootLinks = {
   "Makefile":".build/Makefile",
   "mstr-configs":"apps/helloworld/master-configs",
   "newapp.sh":".build/newapp.sh",
-  "test_LinkFile.py":"tools/test/test_LinkFile.py",
+  "test_LinkFile.py":"tools/tests/test_LinkFile.py",
   }
 
 configsLinks = {
